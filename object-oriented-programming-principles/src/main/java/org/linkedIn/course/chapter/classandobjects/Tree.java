5e953d1d00dc4d181252377f8a3ff436fd5d067b
Vanessa De Jager <vanessa.dejager+gonz@gobss.nz>
2026-02-25 09:59:38 +0200

    Add static and non-static member comments in Tree class to clarify usage

--- object-oriented-programming-principles/src/main/java/org/linkedIn/course/chapter/classandobjects/Tree.java
+++ object-oriented-programming-principles/src/main/java/org/linkedIn/course/chapter/classandobjects/Tree.java
@@ -26,11 +26,16 @@ public class Tree
         this.trunkDiameterInches = this.trunkDiameterInches + 1;
     }
 
+    // static members - is part of a class that is accessible through the class and belongs to the that class.
+    // * It should be an attribute or behavior that's not based on a particular instance,
+    // * But rather belongs to all the instances of that class.
     static void announceTree()
     {
         System.out.println("Look out for that " + TRUNK_COLOR + " tree!");
     }
 
+    // non-static members - is a part of a class that is accessible with an instance and belongs to that instance.
+    // * Means you'll need to create an instance in order to access/use a non-static member.
     void announceTallTree()
     {
         if (this.heightFt > 100) {
